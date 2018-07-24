08354815bb760763db5c7533c9ba1502698e8853
linwenjunid <linwenjunid@163.com>
2018-07-24 02:01:36 -0700

    部署命令

--- manage.py
+++ manage.py
@@ -4,7 +4,7 @@ import os
 from app import create_app,db
 from app.models import User,Role,Post,Follow,Comment
 from flask_script import Manager,Shell
-from flask_migrate import Migrate,MigrateCommand
+from flask_migrate import Migrate,MigrateCommand,upgrade
 
 app=create_app(os.getenv('FLASK_CONFIG') or 'default')
 manager=Manager(app)
@@ -21,6 +21,11 @@ def make_shell_context():
     return dict(app=app,db=db,User=User,Role=Role,Post=Post,Follow=Follow,Comment=Comment)
 manager.add_command('shell',Shell(make_context=make_shell_context))
 manager.add_command('db',MigrateCommand)
+
+@manager.command
+def deploy():
+    upgrade()
+    Role.insert_roles()
 
 if __name__=='__main__':
     manager.run()
