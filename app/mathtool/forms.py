82d8fb5a5d10891aec641fb5851024fa8eb4d995
linwenjunid <linwenjunid@163.com>
2018-07-31 19:56:12 -0700

    测试功能BUG修复

--- app/mathtool/forms.py
+++ app/mathtool/forms.py
@@ -3,6 +3,6 @@ from wtforms import StringField,SubmitField
 from wtforms.validators import DataRequired,Length
 
 class QuestionsForm(FlaskForm):
-    result = StringField('',validators=[Length(0,64)])
+    result = StringField('',validators=[Length(0,64)],default='请输入答案')
     submit = SubmitField('确认')
 
